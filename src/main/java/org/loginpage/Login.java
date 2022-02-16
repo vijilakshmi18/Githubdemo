c54e486de4ebb9546f4d1f415869a5cb81f8a5be
vijilakshmi18 <vijipandiyan18@gmail.com>
2022-02-16 12:26:10 -0800

    Second Commit

--- src/main/java/org/loginpage/Login.java
+++ src/main/java/org/loginpage/Login.java
@@ -1,5 +1,14 @@
 package org.loginpage;
 
 public class Login {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("this is login page use to give email and name");
+		
+		String s="welcome";
+		char c=s.charAt(1);
+		System.out.println(c);
+	}
 
 }
--- src/test/java/org/logintest/LoginTest.java
+++ src/test/java/org/logintest/LoginTest.java
@@ -1,5 +1,9 @@
 package org.logintest;
 
 public class LoginTest {
+	 public static void main(String[] args) {
+		
+		 System.out.println(" this is logi page used to test the valid crendiantials");
+	}
 
 }
